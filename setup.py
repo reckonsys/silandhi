1659e5d583d739740a95690cc423ebe0c99c81ee
dhilipsiva <dhilipsiva@pm.me>
2019-08-27 13:05:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'project',
-    version      = '1.0',
-    packages     = find_packages(),
-    entry_points = {'scrapy': ['settings = silandhi.settings']},
+    name='silandhi',
+    version='1.0',
+    packages=find_packages(),
+    entry_points={'scrapy': ['settings = silandhi.settings']},
 )
